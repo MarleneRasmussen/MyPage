54d7584831f4e2846c675bd77da3c0073b3b854c
MarleneRasmussen <marlene.rasmussen94@gmail.com>
2024-10-03 21:45:09 +0200

    v

--- js/app.js
+++ js/app.js
@@ -1,22 +1,11 @@
-  function filterGallery() {
-  const checkboxes = document.querySelectorAll('.filter-container input[type="checkbox"]');
+function filterGallery(category) {
   const items = document.querySelectorAll('.grid-item');
-  const selectedTopics = Array.from(checkboxes)
-  .filter(checkbox => checkbox.checked && checkbox.value !== 'all')
-  .map(checkbox => checkbox.value);
 
   items.forEach(item => {
-  const topic = item.getAttribute('data-topic');
-  if (selectedTopics.length === 0 || selectedTopics.includes(topic)) {
-  item.style.display = 'block';
-} else {
-  item.style.display = 'none';
-}
-});
-
-  // Handle the "All" checkbox
-  const allCheckbox = document.querySelector('input[value="all"]');
-  if (allCheckbox.checked) {
-  items.forEach(item => item.style.display = 'block');
-}
+    if (category === 'all' || item.getAttribute('data-topic') === category) {
+      item.style.display = 'block'; // Vis elementet
+    } else {
+      item.style.display = 'none'; // Skjul elementet
+    }
+  });
 }
