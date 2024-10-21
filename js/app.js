ce1d73ba09f1fd913fcef8ae5fba2c626a989b2b
MarleneRasmussen <marlene.rasmussen94@gmail.com>
2024-10-21 03:42:33 +0200

    WIP

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function signUp(){
+  const name = document.getElementById('name').value;
+  const date = document.getElementById('date').value;
+
+  alert('Thank you ${name} for signing up. We look forward to seeing you on the ${date}');
+  }
